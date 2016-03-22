f28fb65784df36b56f76a2eaab5df449af24bbb1
Wenhan Chang <sjtu.wenhan@gmail.com>
2016-03-22 17:18:39 -0500

    split files

--- functions_codes.R
+++ functions_codes.R
@@ -1,31 +1,3 @@
-library(mvtnorm)
-library(ggplot2)
-#library(snow)
-
-##location <- "./seq_data_1.txt"
-##location <- c("./seq_data_1.txt", "./seq_data_2.txt", "./seq_data_3.txt")
-location <- c("./seq_data_1.txt", "./seq_data_2.txt", "./seq_data_3.txt", "./seq_data_4.txt", "./seq_data_5.txt")
-
-################################################################
-
-
-if (length(location) == 1) {
-    ## seq_data is a matrix now
-    seq_data <- read_data(location)
-    seq_data <- process_seq_data(seq_data)
-} else if (length(location) > 1) {
-    ## seq_data is a list of matrix now
-    seq_data <- lapply(X = location, FUN = read_data)
-    seq_data <- lapply(X = seq_data, FUN = process_seq_data)
-
-} else {
-    stop("the location of seq data is no valid.")
-}
-
-
-result <- calculate_log_likelihood(0.1)
-result$likelihood <- exp(result$log_likelihood)
-result$likelihood <- result$likelihood / sum(result$likelihood)
 
 
 ################################################################
---
+++ run.R
@@ -0,0 +1,31 @@
+library(mvtnorm)
+library(ggplot2)
+#library(snow)
+
+source("./functions_codes.R")
+##location <- "./seq_data_1.txt"
+##location <- c("./seq_data_1.txt", "./seq_data_2.txt", "./seq_data_3.txt")
+location <- c("./seq_data_1.txt", "./seq_data_2.txt", "./seq_data_3.txt", "./seq_data_4.txt", "./seq_data_5.txt")
+
+################################################################
+
+
+if (length(location) == 1) {
+    ## seq_data is a matrix now
+    seq_data <- read_data(location)
+    seq_data <- process_seq_data(seq_data)
+} else if (length(location) > 1) {
+    ## seq_data is a list of matrix now
+    seq_data <- lapply(X = location, FUN = read_data)
+    seq_data <- lapply(X = seq_data, FUN = process_seq_data)
+
+} else {
+    stop("the location of seq data is no valid.")
+}
+
+
+result <- calculate_log_likelihood(0.1)
+result$likelihood <- exp(result$log_likelihood)
+result$likelihood <- result$likelihood / sum(result$likelihood)
+
+ggplot(data = result, aes(x = s, y = likelihood)) + geom_point() + geom_line() + labs(title = "Likelihood of Different s Values", x = "s values", y = "Likelihood") + theme(plot.title = element_text(lineheight=.8, face="bold"))
